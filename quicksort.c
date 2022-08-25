1d20ee56969fa5e1d921ea05ec8c842b82487610
tstwroot <pablovitorinopanciera@gmail.com>
2022-08-24 20:43:07 -0400

    quicksort release

--- quicksort.c
+++ quicksort.c
@@ -74,30 +74,95 @@ void swap(int *__n1, int *__n2)
     *__n2 = aux;
 }
 
-void quicksort(int *vector, int start, int end)
-{
-	int i = start, j = end-1;
-	int pivo = vector[(start + end) / 2];
+// void quicksort(int *vector, int start, int end)
+// {
+// 	int i = start, j = end-1;
+// 	int pivo = vector[(start + end) / 2];
 
-	while(i <= j)
-	{
-		while(vector[i] < pivo && i < end)
-		{
+// 	while(i <= j)
+// 	{
+// 		while(vector[i] < pivo && i < end) i++;
+// 		while(vector[j] > pivo && j > start) j--;
+// 		if(i <= j)
+// 		{
+//         	swap(vector+i, vector+j);
+// 			i++, j--;
+// 		}
+// 	}
+// 	if(j > start) quicksort(vector, start, j+1);
+// 	if(i < end) quicksort(vector, i, end);
+// }
+
+
+
+
+// void quicksort(int *vector, int start, int end)
+// {
+// 	int i = start, j = end-1;
+// 	int pivo = vector[(start + end) / 2];
+
+// 	while(i <= j)
+// 	{
+// 		while(vector[i] < pivo && i < end)
+// 		{
+// 			i++;
+// 		}
+// 		while(vector[j] > pivo && j > start)
+// 		{
+// 			j--;
+// 		}
+// 		if(i <= j)
+// 		{
+//          swap(vector+i, vector+j);
+// 			i++;
+// 			j--;
+// 		}
+// 	}
+// 	if(j > start)
+// 		quicksort(vector, start, j+1);
+// 	if(i < end)
+// 		quicksort(vector, i, end);
+// }
+
+
+
+
+
+
+
+
+
+/*void quicksort(int *vector, int start, int end)
+{
+	int i = start, j = end - 1; 					// define 2 flags -- i para o inicio do vetor --
+													//  j para o final do vetor
+
+	int pivo = vector[(start + end) / 2]; 			// define o elemento do meio como o pivo
+
+	while (i <= j) 									// o while vai executar enqunto a flag i for menor
+	{			   									// ou igual que a flag j
+
+		while (vector[i] < pivo && i < end) 		// faz a verificação do elemento posição i se ele é menor que
+		{											// que o pivo
 			i++;
 		}
-		while(vector[j] > pivo && j > start)
-		{
+
+		while (vector[j] > pivo && j > start) 		// faz a verificação do elemento posição j se ele é menor que
+		{									  		// que o pivo
 			j--;
+		}~
+
+		if (i <= j)									// se a flag i for menor e j ele chama a função swap
+		{											// que seriaa troca de elementos
+			swap(vector + i, vector + j);
+			i++;									//incrementa o i para poder realizar as interações apos a realizção da troca
+			j--;									//incrementa o j para poder realizar as interações apos a realizção da troca
 		}
-		if(i <= j)
-		{
-            swap(vector+i, vector+j);
-			i++;
-			j--;
-		}
+		
 	}
-	if(j > start)
-		quicksort(vector, start, j+1);
-	if(i < end)
-		quicksort(vector, i, end);
+	if (j > start)									//se casoo j ainda for maior que o inicio ele faz uma chamada recursivo passando 
+		quicksort(vector, start, j + 1);			//o inicio como a referencia e j+1 como o novo final;
+	if (i < end)									//se o i ainda for menor que o final ele passa o inicio comk a posição i
+		quicksort(vector, i, end);					//e o final como final
 }
+*/
